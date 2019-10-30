9944a10a0ebbb37d6bee937c9df65048dc58ddaa
Paulo Henrique <Henrique@Paulos-MacBook-Pro.local>
2019-10-29 23:20:43 -0200

    server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const  path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     mode: 'production',
     entry: './src/main.ts',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        hot: true
+    },
     output: {
         filename: 'app.min.js',
         path: path.join(__dirname, 'dist')
     },
+    plugins: [
+        new CopyPlugin([
+            {
+                from: 'public'
+            }
+        ])
+    ],
     resolve: {
         extensions: ['.ts', '.js']
     },
